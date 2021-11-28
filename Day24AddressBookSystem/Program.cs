c110071ec3103ebe9ad27219d331811dc046c126
akramwasimgithub <imwasim322@gmail.com>
2021-11-28 14:26:06 +0530

    Added: Address book

---
+++ Day24AddressBookSystem/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Day24AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System");
+            /*AddressBook.AddContact();
+            AddressBook.ContactsDisplay();
+*/
+            /* UC3.AddContact();
+             UC3.ContactsDisplay();
+             UC3.EditContact();*/
+
+            /*Delete.AddContact();
+            Delete.ContactsDisplay();
+            Delete.DeleteContact();
+            Delete.ContactsDisplay();*/
+
+
+            /*AddMultipleAddress.addAddressBook();
+            Console.WriteLine("\n");
+            AddMultipleAddress.ContactsDisplay();
+            Console.WriteLine("\n");
+            AddMultipleAddress.EditContact();
+            Console.WriteLine("\n");
+            AddMultipleAddress.ContactsDisplay();
+            Console.WriteLine("\n");*/
+
+            /* UC7DuplicateSearch.addAddressBook();
+             Console.WriteLine("\n");
+             UC7DuplicateSearch.ContactsDisplay();
+             Console.WriteLine("\n");
+
+             UC7DuplicateSearch.EditContact();
+             Console.WriteLine("\n");
+             UC7DuplicateSearch.ContactsDisplay();
+             UC7DuplicateSearch.DeleteContact();*/
+
+            /* UC8_Search.addAddressBook();
+             UC8_Search.PersonSearch();
+ */
+
+
+
+            UC10.addAddressBook();
+            UC10.PersonSearch();
+            
+        }
+    }
+}
